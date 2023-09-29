b10a61052e7373e7a7100f34c286dced285bff60
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-29 11:42:50 -0300

    feat: add Home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from './assets/styles/global'
+import theme from './assets/styles/themes/default'
+
+import Home from './page/Home'
+
 function App() {
-  return <h1>Hello word</h1>
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Home />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App

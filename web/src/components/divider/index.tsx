988723f9ec7b17dfd494d3f9d930c6cdf3c11f2f
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-29 13:01:15 -0300

    feat: add divider components

---
+++ web/src/components/divider/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export const Divider = () => {
+  return <S.Container />
+}
---
+++ web/src/components/divider/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import theme from '../../assets/styles/themes/default'
+
+export const Container = styled.div`
+  border-bottom: 2px solid ${theme.colors.background};
+`

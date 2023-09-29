ec7a5fa4751764df75b05f45d288ef86fec60af0
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-29 01:10:24 -0300

    feat: add cors

---
+++ api/src/middlewares/cors.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173/')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Max-Age', '10')
+  next()
+}

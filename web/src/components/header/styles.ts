ed0957677c305eb19b89c5f3a05cd90e4a499f02
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-30 01:35:41 -0300

    style: add new styles in the header component

--- web/src/components/header/styles.ts
+++ web/src/components/header/styles.ts
@@ -3,11 +3,27 @@ import theme from '../../assets/styles/themes/default'
 
 export const ContainerHeader = styled.header`
   display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 200px;
+  background: ${theme.colors.danger.main};
+`
+
+export const Content = styled.div`
+  display: flex;
   justify-content: center;
   align-items: center;
-
-  background: ${theme.colors.danger.main};
-  height: 200px;
-  width: 100%;
   padding: 0 20px;
+  width: 100%;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: flex-end;
+`
+
+export const Logo = styled.img`
+  display: flex;
+  padding: 20px 20px 50px 20px;
 `

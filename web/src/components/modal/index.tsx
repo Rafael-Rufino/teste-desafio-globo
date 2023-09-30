68025f945b03d36cbf7997c95fb4a426eb89d27d
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-30 01:10:54 -0300

    fix: fix modal props

--- web/src/components/modal/index.tsx
+++ web/src/components/modal/index.tsx
@@ -1,7 +1,7 @@
-import * as S from './styles'
-
 import { Divider } from '../divider'
 import { IHighlight } from '../header'
+
+import * as S from './styles'
 
 interface ModalProps {
   searchResults: IHighlight[]
@@ -17,30 +17,34 @@ export const Modal = ({
   onClose,
 }: ModalProps) => {
   return (
-    <S.ContainerModal onClose={onClose} isOpen={isOpen}>
-      {searchResults.length > 0 && (
-        <S.Header>
-          {searchResults.map((result) => (
-            <S.Link
-              key={result.id}
-              href={result.url}
-              title={`Você será redirecionado para ${result.title}`}
-            >
-              <S.Logo src={result.logo} alt={result.title} />
-              <S.Title>{result.title}</S.Title>
-            </S.Link>
-          ))}
-        </S.Header>
+    <>
+      {isOpen && (
+        <S.ContainerModal onClick={onClose}>
+          {searchResults.length > 0 && (
+            <S.Header>
+              {searchResults.map((result) => (
+                <S.Link
+                  key={result.id}
+                  href={result.url}
+                  title={`Você será redirecionado para ${result.title}`}
+                >
+                  <S.Logo src={result.logo} alt={result.title} />
+                  <S.Title>{result.title}</S.Title>
+                </S.Link>
+              ))}
+            </S.Header>
+          )}
+          <Divider />
+          <S.Wrapper>
+            <S.SuggestionSearch>Sugestões de busca</S.SuggestionSearch>
+          </S.Wrapper>
+          <S.SuggestionsWrapper>
+            {suggestions.map((suggestion) => (
+              <S.Suggestion key={suggestion}>{suggestion}</S.Suggestion>
+            ))}
+          </S.SuggestionsWrapper>
+        </S.ContainerModal>
       )}
-      <Divider />
-      <S.Wrapper>
-        <S.SuggestionSearch>Sugestões de busca</S.SuggestionSearch>
-      </S.Wrapper>
-      <S.SuggestionsWrapper>
-        {suggestions.map((suggestion) => (
-          <S.Suggestion key={suggestion}>{suggestion}</S.Suggestion>
-        ))}
-      </S.SuggestionsWrapper>
-    </S.ContainerModal>
+    </>
   )
 }

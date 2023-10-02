de473d8e829b435adb3ddbd0fe642599504c44e9
RafaelRufino <rafaelrufino.52@gmail.com>
2023-10-02 12:35:18 -0300

    style: adjust styles

--- web/src/components/divider/styles.ts
+++ web/src/components/divider/styles.ts
@@ -4,4 +4,6 @@ import theme from '../../assets/styles/themes/default'
 
 export const Container = styled.div`
   border-bottom: 2px solid ${theme.colors.background};
+  margin-top: 10px;
+  margin-bottom: 10px;
 `

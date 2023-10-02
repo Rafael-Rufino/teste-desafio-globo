320790ced22b3ff2ed1e116282c8d77c98095a02
RafaelRufino <rafaelrufino.52@gmail.com>
2023-10-02 12:57:38 -0300

    style: refactor styles searcModal

--- web/src/components/searchModal/styles.ts
+++ web/src/components/searchModal/styles.ts
@@ -6,17 +6,23 @@ export const ContainerModal = styled.div`
   z-index: 1000;
   top: 0;
   margin-top: 170px;
-  max-width: 400px;
+
+  max-width: 540px;
   width: 100%;
-  height: 400px;
+
+  height: 100%;
+  max-height: 500px;
   overflow: hidden;
 
   border-radius: 8px;
   background: ${theme.colors.white};
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.09);
+  padding: 10px 0;
+
+  overflow-y: auto;
 `
 export const Header = styled.header`
-  padding: 20px;
+  padding: 20px 20px 0 20px;
 `
 
 export const Link = styled.a`
@@ -24,36 +30,59 @@ export const Link = styled.a`
   align-items: center;
   text-decoration: none;
   gap: 0.75rem;
+  color: ${theme.colors.gray.light};
+
+  strong {
+    color: ${theme.colors.gray.dark};
+  }
 
   :hover {
     opacity: 0.8;
   }
 `
 
-export const Title = styled.h1`
+export const Name = styled.h1`
   font-size: ${theme.fonts.sizes.xLarge};
   font-weight: ${theme.fonts.weight.bold};
   color: ${theme.colors.gray.dark};
 `
 
 export const Logo = styled.img`
-  height: 40px;
+  height: 2.5em;
 `
 export const Wrapper = styled.div`
   display: flex;
   justify-content: end;
-  padding: 10px 20px;
-`
-export const SuggestionSearch = styled.small`
-  color: ${theme.colors.gray.light};
-  font-size: ${theme.fonts.sizes.xLarge};
+  padding: 0px 20px 10px 20px;
 `
 export const SuggestionsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 0 20px;
 `
-export const Suggestion = styled.div`
-  font-size: ${theme.fonts.sizes.xLarge};
+export const Suggestion = styled.small`
+  color: ${theme.colors.gray.light};
+  font-size: ${theme.fonts.sizes.large};
+  font-style: italic;
+
+  @media (max-width: 480px) {
+    font-size: ${theme.fonts.sizes.medium};
+  }
+`
+export const SuggestionSearch = styled.div`
+  font-size: ${theme.fonts.sizes.large};
   color: ${theme.colors.gray.dark};
+
+  @media (max-width: 480px) {
+    font-size: ${theme.fonts.sizes.small};
+  }
+`
+
+export const SearchResultNotFound = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: ${theme.fonts.sizes.xLarge};
+  color: ${theme.colors.gray.light};
 `

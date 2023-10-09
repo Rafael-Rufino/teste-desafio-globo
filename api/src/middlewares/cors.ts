cbcdce9b63002fae1e3ee45f8fa2c27b8873eaf2
RafaelRufino <rafaelrufino.52@gmail.com>
2023-10-09 13:30:55 -0300

    refactor: add generic ip

--- api/src/middlewares/cors.ts
+++ api/src/middlewares/cors.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
 
 export const cors = (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173')
+  const allowedOrigins = ['http://0.0.0.0:5173', req.headers.origin]
+
+  if (allowedOrigins.includes(req.headers.origin)) {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin as string)
+  }
   res.setHeader('Access-Control-Allow-Methods', '*')
   res.setHeader('Access-Control-Allow-Headers', '*')
   res.setHeader('Access-Control-Max-Age', '10')

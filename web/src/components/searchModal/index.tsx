8b06f3a42330a06021a5bcd53b8282aca43d5188
RafaelRufino <rafaelrufino.52@gmail.com>
2023-10-09 03:15:50 -0300

    refactor: refactor searchModal component

--- web/src/components/searchModal/index.tsx
+++ web/src/components/searchModal/index.tsx
@@ -1,12 +1,12 @@
 import { FiChevronRight } from 'react-icons/fi'
-import { Divider } from '../divider'
-import { IHighlight } from '../header'
-
 import * as S from './styles'
+
+import { Divider } from '../divider'
+import { IHighlight, ISuggestion } from '../header/interface'
 
 export interface SearchModalProps {
   searchResults: IHighlight[]
-  suggestions: string[]
+  suggestions: ISuggestion[]
   suggestionValue: string
   isOpen: boolean
   onClose: () => void
@@ -21,79 +21,70 @@ export const SearchModal = ({
 }: SearchModalProps) => {
   const hasSearchResults = searchResults.length > 0
 
-  const hasValidSuggestionValue = suggestionValue.length > 0
-
   return (
     <>
       {isOpen && (
         <S.ContainerModal role="modal" onClick={onClose}>
           {hasSearchResults && (
-            <S.Header>
-              {searchResults.map((result) => (
-                <S.Link
-                  key={result.id}
-                  href={result.url}
-                  title={`Você será redirecionado para página do Globo.com com a busca  ${result.title}`}
-                >
-                  <S.Logo src={result.logo} alt={result.title} />
-                  <S.Name>{result.title}</S.Name>
-                </S.Link>
-              ))}
-            </S.Header>
-          )}
-
-          {hasSearchResults && (
             <>
+              <S.Header>
+                {searchResults.map((result) => (
+                  <S.Link
+                    key={result.id}
+                    href={result.url}
+                    title={`Você será redirecionado para página do Globo.com com a busca ${result.title}`}
+                  >
+                    <S.Logo src={result.logo} alt={result.title} />
+                    <S.Name>{result.title}</S.Name>
+                  </S.Link>
+                ))}
+              </S.Header>
+
               <Divider />
+
               <S.Wrapper>
                 <S.Suggestion>Sugestões de busca</S.Suggestion>
               </S.Wrapper>
 
               <S.SuggestionsWrapper>
-                {suggestions.map((suggestion) => (
-                  <S.SuggestionSearch key={suggestion}>
-                    <strong>{suggestion}</strong>
+                {suggestions.map((suggestion: ISuggestion) => (
+                  <S.SuggestionSearch key={suggestion.id}>
+                    <strong>{suggestion.value}</strong>
                   </S.SuggestionSearch>
                 ))}
               </S.SuggestionsWrapper>
 
               <Divider />
 
-              {hasValidSuggestionValue && (
-                <>
-                  <S.SuggestionsWrapper>
-                    <S.SuggestionSearch>
-                      <S.Link
-                        href={`http://g1.globo.com/busca/?q=${encodeURIComponent(
-                          suggestionValue
-                        )}`}
-                        target="_blank"
-                        title={`Você será redirecionado para página do Globo.com com a busca ${suggestionValue}`}
-                      >
-                        buscar <strong>'{suggestionValue}'</strong>na Globo.com
-                        <FiChevronRight size={14} />
-                      </S.Link>
-                    </S.SuggestionSearch>
-                  </S.SuggestionsWrapper>
+              <S.SuggestionsWrapper>
+                <S.SuggestionSearch>
+                  <S.Link
+                    href={`http://g1.globo.com/busca/?q=${encodeURIComponent(
+                      suggestionValue
+                    )}`}
+                    title={`Você será redirecionado para página do Globo.com com a busca ${suggestionValue}`}
+                  >
+                    buscar <strong>'{suggestionValue}'</strong>na Globo.com
+                    <FiChevronRight size={14} />
+                  </S.Link>
+                </S.SuggestionSearch>
+              </S.SuggestionsWrapper>
 
-                  <Divider />
+              <Divider />
 
-                  <S.SuggestionsWrapper>
-                    <S.SuggestionSearch>
-                      <S.Link
-                        href={`https://www.google.com/search?q=${encodeURIComponent(
-                          suggestionValue
-                        )}`}
-                        target="_blank"
-                        title={`Você será redirecionado para página do Google com a busca ${suggestionValue}`}
-                      >
-                        buscar <strong>'{suggestionValue}'</strong> na Web
-                        <FiChevronRight size={14} />
-                      </S.Link>
-                    </S.SuggestionSearch>
-                  </S.SuggestionsWrapper>
-                </>
-              )}
+              <S.SuggestionsWrapper>
+                <S.SuggestionSearch>
+                  <S.Link
+                    href={`https://www.google.com/search?q=${encodeURIComponent(
+                      suggestionValue
+                    )}`}
+                    title={`Você será redirecionado para página do Google com a busca ${suggestionValue}`}
+                  >
+                    buscar <strong>'{suggestionValue}'</strong> na Web
+                    <FiChevronRight size={14} />
+                  </S.Link>
+                </S.SuggestionSearch>
+              </S.SuggestionsWrapper>
             </>
           )}
 

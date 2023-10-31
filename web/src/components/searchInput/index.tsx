14f334112a9fa66048cd9ed9edcb41971c5dd8bd
rafael rufino <rafaelrufino.52@email.com>
2023-10-31 03:55:09 -0300

    refator: refactor SearchInput component

--- web/src/components/searchInput/index.tsx
+++ web/src/components/searchInput/index.tsx
@@ -4,14 +4,20 @@ import React from 'react'
 type SearchInputProps = {
   icon?: JSX.Element
   inputRef?: React.Ref<HTMLInputElement>
+  type?: string
 } & React.InputHTMLAttributes<HTMLInputElement>
 
 export const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
-  ({ icon, inputRef, ...rest }) => {
+  ({ icon, type = 'text', ...rest }, ref) => {
     return (
       <S.Container>
         {icon && <S.Icon>{icon}</S.Icon>}
-        <S.Input placeholder="Digite sua busca" ref={inputRef} {...rest} />
+        <S.Input
+          placeholder="Digite sua busca"
+          type={type}
+          ref={ref}
+          {...rest}
+        />
       </S.Container>
     )
   }
